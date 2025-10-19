6245390c66e88e8cf28d416e84648fe20b29d0db
ALI200407 <atallahali684@gmail.com>
2025-10-19 15:31:45 +0100

    Update t01.c

--- t01.c
+++ t01.c
@@ -1,2 +1,59 @@
-.L.M.;m.M
-pptllplp
+#include<stdio.h>
+int i,j,h,g,n;
+  
+
+int main()
+{
+    printf("enter n");
+    scanf("%d",&n);
+    int T[n][n];
+     
+     for(i=0;i<n;i++)
+	 {
+     	 for(j=0;j<n;j++)
+	     {
+     	        printf("t[%d][%d]",i,j);
+     			scanf("%d",&T[i][j]);
+	     }
+     	
+	 }
+	        for(i=0;i<n;i++)
+	        {
+     	          for(j=0;j<n;j++)
+	              {
+     	                 printf("%d",T[i][j]);
+     			         printf("\t");
+	               }
+         	printf("\n");
+	        }
+	     
+	       for(i=0;i<n;i++)
+	       {
+     	        for(j=0;j<n;j++)
+	            {
+     	              if(i==j)
+					  {
+					  	T[i][j]=T[i][j];
+					  }
+					    	
+					     if(i!=j)
+					     {
+					  	      T[i][j]=T[j][i];
+					     }
+					    
+     		        	
+	            }
+     	
+	        }
+	        
+	                                for(i=0;i<n;i++)
+	       							{
+     	        							for(j=0;j<n;j++)
+	            							{
+     	              							 printf("%d",T[i][j]);
+     			         							printf("\t");
+     		        	
+	           								 }
+     										printf("\n");
+	       							 }
+}
